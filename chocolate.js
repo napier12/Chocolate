5d0fe9eab43bb308b8e9a22b7274a0f42b6a61aa
napier12 <napier12@purdue.edu>
2025-06-22 20:51:43 -0500

    Add files via upload

---
+++ chocolate.js
@@ -0,0 +1,18 @@
+/** @format */
+
+let showCal = true;
+
+const showCalories = () => {
+  console.log("toggle");
+  showCal = !showCal;
+  if (showCal) $("p.calories").show();
+  else $("p.calories").hide();
+};
+
+const highlightRandomChocolate = () => {
+  $(".wrapper .box").css("background-color", "#f2f4d4");
+  let randomIndex = Math.floor(Math.random() * 50) + 1;
+  alert("Bring this number to the cashier: " + randomIndex);
+  randomIndex = randomIndex % 10;
+  $(".wrapper .box:nth-child(" + randomIndex + ")").css("background-color", "yellow");
+};
